e9c8ba908eae59227cb94bbfd591ac9a9e020061
Paul Drauz-Brown <paul.drauzbrown@icloud.com>
2017-10-28 00:52:04 +0800

    Adding wwise audio and starting integration code

---
+++ Ucenter-Game-Jam_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,66 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID CAR_FORWARD = 627693719U;
+        static const AkUniqueID CAR_HORN = 4160219795U;
+        static const AkUniqueID CAR_STOP = 1465464336U;
+        static const AkUniqueID CAR_TURN = 3370073757U;
+        static const AkUniqueID GAME_START = 733168346U;
+        static const AkUniqueID START_ENGINE = 1300784916U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSIC
+        {
+            static const AkUniqueID GROUP = 3991942870U;
+
+            namespace STATE
+            {
+                static const AkUniqueID MUSIC_1 = 508837814U;
+                static const AkUniqueID MUSIC_2 = 508837813U;
+                static const AkUniqueID MUSIC_3 = 508837812U;
+                static const AkUniqueID MUSIC_4 = 508837811U;
+                static const AkUniqueID START = 1281810935U;
+            } // namespace STATE
+        } // namespace MUSIC
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID CAMERA_HEIGHT = 587070286U;
+        static const AkUniqueID CAR_SPEED = 4164958897U;
+        static const AkUniqueID FRUSTRATION = 2499973362U;
+        static const AkUniqueID FUEL = 2661514705U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
